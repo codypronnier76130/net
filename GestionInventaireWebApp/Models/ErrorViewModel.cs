9b50dc8f74677b8de1d0c3e42c6f8598594ecc41
Cody <codypronnier@gmail.com>
2021-03-08 10:17:10 +0100

    Ajout projet WebApp

---
+++ GestionInventaireWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace GestionInventaireWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
