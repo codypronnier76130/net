7eb8bbd591ffab6709554dd06ed73056ac7a52c3
Cody <codypronnier@gmail.com>
2021-03-08 11:06:48 +0100

    Ajout packages

---
+++ GestionInventaireWebApp/Models/BDD/Utilisateur.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestionInventaireWebApp.Models.BDD
+{
+    public class Utilisateur
+    {
+    }
+}
