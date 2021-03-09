a24513e0671a1f1716b10417e64aa7c1559b96d7
Louvel <noe.louvel@viacesi.fr>
2021-03-09 15:41:04 +0100

    Début API

---
+++ NetAPI/Models/Article.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GestionInventaireWebApp.Models.BDD
+{
+    public class Article
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public DateTime CreateDate { get; set; }
+        public int BuyPrice { get; set; }
+        public int SellPrice { get; set; }
+        public Provider Provider { get; set; }
+        public Type Type { get; set; }
+        public ICollection<OrderArticle> OrderArticles { get; set; }
+        public Stock Stock { get; set; }
+        public Admin Admin { get; set; }
+    }
+}
