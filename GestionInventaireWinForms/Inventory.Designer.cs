4eb7c92d1b5892309280a584a460e7347cd23ad5
Acetann <paulfremont27@gmail.com>
2021-03-11 14:28:07 +0100

    Winform Inventory

---
+++ GestionInventaireWinForms/Inventory.Designer.cs
@@ -0,0 +1,312 @@
+﻿
+namespace GestionInventaireWinForms
+{
+    partial class Inventory
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(209, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Vin Rouge";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(209, 181);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(58, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Pétillant";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(209, 223);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(55, 17);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Digestif";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(209, 93);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(67, 17);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "Vin Blanc";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(209, 139);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(41, 17);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Rosé";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(299, 54);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(195, 22);
+            this.textBox1.TabIndex = 22;
+            this.textBox1.Text = "2";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(299, 93);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(195, 22);
+            this.textBox2.TabIndex = 23;
+            this.textBox2.Text = "50";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(299, 139);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(195, 22);
+            this.textBox3.TabIndex = 24;
+            this.textBox3.Text = "20";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(299, 181);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(195, 22);
+            this.textBox4.TabIndex = 25;
+            this.textBox4.Text = "4";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(299, 223);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(195, 22);
+            this.textBox5.TabIndex = 26;
+            this.textBox5.Text = "0";
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(495, 47);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 36);
+            this.button1.TabIndex = 27;
+            this.button1.Text = "Disponible ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(495, 90);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(106, 36);
+            this.button2.TabIndex = 28;
+            this.button2.Text = "Disponible";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(495, 132);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(106, 36);
+            this.button3.TabIndex = 29;
+            this.button3.Text = "Disponible";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(495, 174);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(106, 36);
+            this.button4.TabIndex = 30;
+            this.button4.Text = "Disponible";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(495, 216);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(106, 36);
+            this.button5.TabIndex = 31;
+            this.button5.Text = "Disponible";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(361, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 17);
+            this.label2.TabIndex = 32;
+            this.label2.Text = "Quantité";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(607, 47);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(106, 36);
+            this.button6.TabIndex = 33;
+            this.button6.Text = "Remplir";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(607, 90);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(106, 36);
+            this.button7.TabIndex = 34;
+            this.button7.Text = "Remplir";
+            this.button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(607, 132);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(106, 36);
+            this.button8.TabIndex = 35;
+            this.button8.Text = "Remplir";
+            this.button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            this.button9.Location = new System.Drawing.Point(607, 174);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(106, 36);
+            this.button9.TabIndex = 36;
+            this.button9.Text = "Remplir";
+            this.button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(607, 216);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(106, 36);
+            this.button10.TabIndex = 37;
+            this.button10.Text = "Remplir";
+            this.button10.UseVisualStyleBackColor = true;
+            // 
+            // Inventory
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1014, 529);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Name = "Inventory";
+            this.Load += new System.EventHandler(this.Inventory_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+    }
+}
---
+++ GestionInventaireWinForms/Inventory.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GestionInventaireWinForms
+{
+    public partial class Inventory : Form
+    {
+        public Inventory()
+        {
+            InitializeComponent();
+        }
+        
+
+        private void Inventory_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int quantity;
+            Int32.TryParse(textBox1.Text, out quantity);
+            if (quantity > 0)
+            {
+                MessageBox.Show("En stock : " + quantity);
+            }
+            else
+            {
+                MessageBox.Show("En Rupture");
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int quantity;
+            Int32.TryParse(textBox2.Text, out quantity);
+            if (quantity > 0)
+            {
+                MessageBox.Show("En stock : " + quantity);
+            }
+            else
+            {
+                MessageBox.Show("En Rupture");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int quantity;
+            Int32.TryParse(textBox3.Text, out quantity);
+            if (quantity > 0)
+            {
+                MessageBox.Show("En stock : " + quantity);
+            }
+            else
+            {
+                MessageBox.Show("En Rupture");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int quantity;
+            Int32.TryParse(textBox4.Text, out quantity);
+            if (quantity > 0)
+            {
+                MessageBox.Show("En stock : " + quantity);
+            }
+            else
+            {
+                MessageBox.Show("En Rupture");
+            }
+        }
+        private void button5_Click(object sender, EventArgs e)
+        {
+            int quantity;
+            Int32.TryParse(textBox5.Text, out quantity);
+            if (quantity > 0)
+            {
+                MessageBox.Show("En stock : " + quantity);
+            }
+            else
+            {
+                MessageBox.Show("En Rupture");
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int stock;
+            Int32.TryParse(textBox1.Text, out stock);
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
